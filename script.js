eb157cb7632bcfd4f7f2dc9ae3644296bff3d6e3
DarBaala <joker.milan1997gmail.com>
2022-01-10 19:21:40 +0300

    upd

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 "use strict";
-
-let openingString = "Какая-то строка";
 function checkStr(checkMessage) {
   if (typeof checkMessage === "string") {
     if (checkMessage.trim().length > 30) {
-      openingString = checkMessage.trim().substring(0, 30) + "...";
+      console.log(checkMessage.trim().substring(0, 30) + "...");
+    } else {
+      console.log(checkMessage.trim());
     }
   } else {
     console.log("Передана не строка, ты втираешь мне какую-то дичь?");
   }
-  return openingString;
 }
-console.log("checkStr(): ", checkStr("Строка менее 30 символов"));
-console.log(
-  "checkStr(): ",
-  checkStr(
-    "Строка больше 30 символов символов символов символов символов символов символов символов"
-  )
+checkStr("Строка менее 30 символов");
+checkStr(
+  "Строка больше 30 символов символов символов символов символов символов символов символов"
 );
-console.log("checkStr(): ", checkStr(303));
+checkStr(303);
