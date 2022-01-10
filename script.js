01204512de2a8ac0dd18525e9b97ee72228b4105
DarBaala <joker.milan1997gmail.com>
2022-01-10 18:09:02 +0300

    upd

--- script.js
+++ script.js
@@ -3,14 +3,18 @@
 let openingString = "Какая-то строка";
 function checkStr(checkMessage) {
   if (typeof checkMessage === "string") {
-    let line = checkMessage.trim();
-    if (line.length > 30) {
-      openingString = line.substring(0, 30) + "...";
+    if (checkMessage.trim().length > 30) {
+      openingString = checkMessage.trim().substring(0, 30) + "...";
     } else {
-      openingString = line;
       console.log("Передана не строка, ты втираешь мне какую-то дичь?");
     }
   }
   return openingString;
 }
-console.log("checkStr():", checkStr("Строка для проверки"));
+console.log("checkStr(): ", checkStr("Строка менее 30 символов"));
+console.log(
+  "checkStr(): ",
+  checkStr(
+    "Строка больше 30 символов символов символов символов символов символов символов символов"
+  )
+);
