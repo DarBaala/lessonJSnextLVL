77a6bd4a74a35e316d7cb92ea46bd40f09314b33
DarBaala <joker.milan1997gmail.com>
2022-01-10 17:40:28 +0300

    upd

--- script.js
+++ script.js
@@ -66,3 +66,18 @@ console.log(
     ? "Преподовадель"
     : "студент"
 );
+
+function checkPrompt(checkMessage) {
+  let openingString = "Какая-то строка";
+  if (typeof checkMessage === "string") {
+    let maxStrong = 30;
+    let line = checkMessage.trim();
+    if (line.length > maxStrong) {
+      openingString = line.substring(0, maxStrong) + "...";
+    } else {
+      openingString = line;
+    }
+  }
+  return openingString;
+}
+console.log("checkPrompt(checkMessage):", checkPrompt(checkMessage));
